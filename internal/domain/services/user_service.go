79789294e48796b599e5582937506dfb8a660a59
Marcelo Prates <mpratesjunior@gmail.com>
2024-10-18 20:34:22 -0300

    wip: create user use case

---
+++ internal/domain/services/user_service.go
@@ -0,0 +1,40 @@
+package services
+
+import (
+	"context"
+	"github.com/google/uuid"
+	"go-auth-api/internal/domain/adapters"
+	"go-auth-api/internal/domain/dtos"
+	"go-auth-api/internal/domain/exceptions"
+	"go-auth-api/internal/domain/types"
+)
+
+type userServiceImpl struct {
+	userPersistence adapters.UserPersistence
+}
+
+func UserServiceConstructor(userPersistence adapters.UserPersistence) adapters.UserService {
+	return &userServiceImpl{
+		userPersistence: userPersistence,
+	}
+}
+
+func (u userServiceImpl) NewUser(ctx context.Context, user *dtos.UsersDto) (*types.User, exceptions.ErrorType) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (u userServiceImpl) UpdateUser(ctx context.Context, data *dtos.UpdateUserDto, id uuid.UUID) (*types.User, exceptions.ErrorType) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (u userServiceImpl) ListUser(ctx context.Context, params dtos.QueryParams) (*[]types.User, exceptions.ErrorType) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (u userServiceImpl) FindUserById(ctx context.Context, id *uuid.UUID) (*types.User, exceptions.ErrorType) {
+	//TODO implement me
+	panic("implement me")
+}
